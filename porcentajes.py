092ee2b3a3edb8559eb8030a238e522d4cae585b
Gerardo ValQ <A01374994@itesm.mx>
2017-08-16 21:45:46 -0500

    Gerardo Valderrama Update

--- porcentajes.py
+++ porcentajes.py
@@ -1,7 +1,15 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Gerardo Arturo Valderrama Quiroz
+# Descripcion: Dar el total de alumnos y el porcentaje de hombres y mujeres de acuerdo a los datos proporcionados por el usuario
 
 # A partir de aquí escribe tu programa
 
+numhombres = input("Da el número de alumnos inscritos hombres: ")
+nummujeres = input("Da el número de alumnos inscritos mujeres: ")
+total = int(numhombres)+int(nummujeres)
+mujerespor = int(nummujeres)*100/int(total)
+hombrespor = int(numhombres)*100/int(total)
+print("El total de alumnos inscritos es de: ", total)
+print("El porcentaje de mujeres inscritas es de: ", mujerespor,"%")
+print("El porcentaje de hombres inscritos es de: ", hombrespor,"%")
