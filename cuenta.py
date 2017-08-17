69dc5bfa60c2f8db5c1e6e6912014b655ac115fd
Gerardo ValQ <A01374994@itesm.mx>
2017-08-16 21:26:49 -0500

    Gerardo Valderrama Update

--- cuenta.py
+++ cuenta.py
@@ -1,7 +1,16 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Gerardo Arturo Valderrama Quiroz, A01374994
+# Descripcion: De acuerdo al costo de una comida, darle al usuario su subtotal, la propina que debe de dar, el IVA y el total de la comida
 
 # A partir de aquí escribe tu programa
 
+costo = input("Inserta el costo de la comida: ")
+subtotal = costo
+propina = float(costo)*.12
+iva = float(costo)*.16
+total= float(subtotal)+propina+iva
+print("Su subtotal es de $", subtotal)
+print("La propina es de $", propina)
+print("El IVA es de $", iva)
+print("Su Total es de $", total)
