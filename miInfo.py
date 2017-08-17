63ab6387ba2d91a9c2a800631fc2bbc7ed3c0a8c
Gerardo Valderrama <A01374994@itesm.mx>
2017-08-17 12:50:13 -0500

    Ejercicio 1 Completado

--- miInfo.py
+++ miInfo.py
@@ -5,8 +5,9 @@
 
 # A partir de aquí escribe tu programa
 
-Print("Nombre: Gerardo Arturo Valderrama Quiroz")
-Print("Matricula: A01374994")
-Print("Carrera: ISC - Ingeneria en Sistemas Computacionales")
-Print("Escuela de procedencia: Prepa Tec Bicultural")
-Print("Descripcion: Me gusta mucho leer, sobretodo de fantasia, ciencia ficción y aventura, los videojuegos, mi favorito es The legend of Zelda, Me gusta la comida de todo el mundo y me gusta cocinar.")
+print("Nombre: Gerardo Arturo Valderrama Quiroz")
+print("Matricula: A01374994")
+print("Carrera: ISC - Ingeneria en Sistemas Computacionales")
+print("Escuela de procedencia: Prepa Tec Bicultural")
+print("""Descripcion: Me gusta mucho leer, sobretodo de fantasia, ciencia ficción y aventura, los videojuegos,
+             mi favorito es The legend of Zelda, Me gusta la comida de todo el mundo y me gusta cocinar.""")
