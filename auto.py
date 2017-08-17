a3cb632461dcc5c892e957527e9382908c0b788e
Gerardo ValQ <A01374994@itesm.mx>
2017-08-16 21:06:10 -0500

    Gerardo Valderrama Update

--- auto.py
+++ auto.py
@@ -1,7 +1,14 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Gerardo Arturo Valderrama Quiroz, A01374994
+# Descripcion: Calcular determinada distancia y tiempo dada una velocidad por el usuario
 
 # A partir de aquí escribe tu programa
 
+vel = input("Inserta una velocidad: ")
+distancia6hr = vel*6
+distancia10hr = vel*10
+tiempo500km = 500/vel
+print("Tu distancia en 6 horas es de ", distancia6hr, " km")
+print("Tu distancia en 10 horas es de ", distancia10hr, " km")
+print("Tu tiempo al recorrer 500 kilometros es de ", tiempo500km, " horas")
