1a07331a1d98bdec0675db59548b20709ccec563
luismibaq <30807795+luismibaq@users.noreply.github.com>
2017-08-16 22:47:25 -0500

    Add files via upload

--- coordenadas.py
+++ coordenadas.py
@@ -1,1 +1,27 @@
+#encoding: UTF-8
 
+# Autor: Luis Miguel Baqueiro Vallejo, A01745997
+# Descripcion: Problema 5
+import math
+
+x = int(input("Valor de x: "))
+y = int(input("Valor de y: "))
+magnitud = (((x**2) + (y**2))**(1/2))
+if x > 0 and y > 0:
+    x = math.fabs(x)
+    y = math.fabs(y)
+    angulo = (math.atan(y/x)) * (180/math.pi)
+if x > 0 and y < 0:
+    x = math.fabs(x)
+    y = math.fabs(y)
+    angulo = ((math.atan(y / x)) * (180 / math.pi)) + 270
+if x < 0 and y < 0:
+    x = math.fabs(x)
+    y = math.fabs(y)
+    angulo = ((math.atan(y / x)) * (180 / math.pi)) + 180
+if x < 0 and y > 0:
+    x = math.fabs(x)
+    y = math.fabs(y)
+    angulo = ((math.atan(y / x)) * (180 / math.pi)) + 90
+print("Magnitud: ",magnitud)
+print("Angulo: ", angulo)
