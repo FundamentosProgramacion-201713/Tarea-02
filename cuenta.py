469bde52436993984a2c5b3107d4980f2743cc89
Jean Paul Esquivel Lobato <A01376152@itesm.mx>
2017-08-18 09:53:04 -0500

    Subo pruba para asegurarme que este vinculado Github con PyCharm

--- cuenta.py
+++ cuenta.py
@@ -1,6 +1,6 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
+# Autor: Jean Paul Esquivel Lobato,   A01376152
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # A partir de aquí escribe tu programa
