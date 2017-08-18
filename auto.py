a9ecc57446ed8c5072c1c500941ea0b5bbb89fc6
Carlos Pano <carlospano8@gmail.com>
2017-08-18 18:18:28 -0500

    Programa "miInfo" y "Auto" terminado. Faltan 3...

--- auto.py
+++ auto.py
@@ -1,7 +1,24 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
+# Autor: Carlos Pano Hernandez, A01066264
+# Descripcion: Se imprime la distancia recorrida en 6 y 10 hrs con cualquier velocidad en Km/h. Se imprime de igual manera el tiempo que toma recorrer 500 km.
 # A partir de aquí escribe tu programa
+
+#Paso 1: Introducir velocidad
+velocidadValor= input ("Introduce la velocidad del coche:")
+velocidad = int(velocidadValor)
+
+#Paso 2: 6 hrs
+distanciaSeis = velocidad*6
+
+#Paso 3: 10 hrs
+distanciaDiez = velocidad*10
+
+#Paso 4: Tiempo para 500Km
+tiempoQuinientos = 500/velocidad
+
+#Paso 4: Imprimir 2, 3 y 4
+print ("Distancia recorrida en 6 hrs:",distanciaSeis, "km")
+print ("Distancia recorrida en 10 hrs:", distanciaDiez, "km")
+print ("Tiempo para recorrer 500 km:", tiempoQuinientos, "hrs.")
 
