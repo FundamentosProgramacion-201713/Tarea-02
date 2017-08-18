7ea26c511e46fefc82b2fbd5890f6a97a311d3f3
Carlos Pano H <carlospano8@gmail.com>
2017-08-18 09:52:15 -0500

    Primera parte terminada. Falta resolver lo demás.

--- miInfo.py
+++ miInfo.py
@@ -1,7 +1,9 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Carlos Pano Hernandez, A01066264
+# Descripcion: Se imprime mi informacion personal: Nombre, matricula, carrera, escuela y descripcion.
 
 # A partir de aquí escribe tu programa
 
+print ("Mi nombre es:", "Carlos Pano Hernandez")
+print ("Mi matrIcula es:", "A01066264")
