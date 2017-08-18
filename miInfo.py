f425c0bbec47e29145e4c13978755f723f0e1d2c
A01746139 <luisfersurfer14@hotmail.com>
2017-08-18 09:51:54 -0500

    Prueba en el archivo miinfo.

--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,8 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
+# Autor: Luis Fernando Figueroa Rendon
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # A partir de aquí escribe tu programa
 
-print("Hola")   # borrar
+print("Nombre: Luis Fernando Figueroa Rendon")   # borrar
