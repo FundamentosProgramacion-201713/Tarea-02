8591609daa21bfc8dbe196ea49a1e4a8aad6d1c2
Carlos Pano <carlospano8@gmail.com>
2017-08-18 17:50:20 -0500

    Programa "miInfo" terminado. Faltan 4...

--- miInfo.py
+++ miInfo.py
@@ -7,3 +7,7 @@
 
 print ("Mi nombre es:", "Carlos Pano Hernandez")
 print ("Mi matrIcula es:", "A01066264")
+print ("Mi carrera es:", "ISDR, en otras... Ingeniería en Sistemas Digitales y Robótica")
+print ("Escuela de procedencia:", "Vengo de la PrepaTec campus Esmeralda")
+print ("Un poco sobre mi:", """Aparte de la programación y Tecnología, me encanta el diseño gráfico y la fotografía. Disfruto ver todo tipo de películas.
+También me encanta estar con mis amigos, ir de fiesta y pasarla bien. Me gusta mucho la música electrónica y también de alguna manera el estudio.""")
