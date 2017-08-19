3e2cb51a27740a3cfcc5c419643e81a6f5b7b368
Carlos Pano <carlospano8@gmail.com>
2017-08-18 20:34:44 -0500

    TODOS los problemas terminados.

--- Coordenadas.py
+++ Coordenadas.py
@@ -3,3 +3,24 @@
 # Autor: Carlos Pano Hernández, A01066264
 # Descripcion: COnvertidor de coordenadas cartesianas a coordenadas polares.
 # A partir de aquí escribe tu programa
+
+import math
+
+#Introducir "x" y "y"
+valorX = input("x:")
+valorX = int(valorX)
+
+valorY = input("y:")
+valorY = int(valorY)
+
+#Angulo
+anguloEnRadianes = math.atan2(valorY,valorX)
+angulo = (anguloEnRadianes*180)/3.1416
+
+#Magnitud
+magnitud = ((valorX*valorX)+(valorY*valorY))**0.5
+
+#Imprimir resultados
+print ("---------------------")
+print ("Magnitud:", magnitud)
+print ("ángulo:", angulo)
