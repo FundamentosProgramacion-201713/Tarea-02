97d9d14e3a409ab5e90916a2aa6c8c87fbfdc7be
A01375560 Iván Alejandro Dumas Martínez <30930932+A01375560@users.noreply.github.com>
2017-08-19 15:45:02 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,7 +1,19 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Ivan Alejandro Dumas Martinez, A01375560
+# Descripcion: Este programa muestra el recorrido de un carro despues de 6 y 10 horas, así como el tiempo que necesita para
+# recorrer 500 km; de acuerdo a la velocidad introducida por el usuario.
 
 # A partir de aquí escribe tu programa
+#1
+vel = int(input("A qué velocidad va el auto en km/h?"))
 
+#2
+hor6 = vel * 6
+hor10 = vel * 10
+dist500 = 500/vel
+
+#3
+print ("A las 6 horas el auto recorre:", str(hor6), "km")
+print ("A las 10 horas el auto recorre:", str(hor10), "km")
+print ("El auto recorre 500 km en:", str(dist500), "horas")
