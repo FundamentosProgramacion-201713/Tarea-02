eb9208c89ed611dedfd5f5dfcf4f3165b82cb305
Carlos Pano <carlospano8@gmail.com>
2017-08-18 19:48:11 -0500

    TODOS los problemas terminados.

--- porcentajes.py
+++ porcentajes.py
@@ -1,7 +1,30 @@
 #encoding: UTF-8
 
 # Autor: Carlos Pano Hernandez, A01066264
-# Descripcion: Calculadora de porcentaje de hombres y mujeres en un salón.
+# Descripcion: Calculadora del porcentaje de hombres y mujeres en un salón.
 
 # A partir de aquí escribe tu programa
 
+#Número de hombres y mujeres.
+hombres = input("Hombres inscritos:")
+hombres = int(hombres)
+
+mujeres = input("Mujeres inscritas:")
+mujeres = int(mujeres)
+
+#Suma
+totalSalon = hombres + mujeres
+totalSalon = int(totalSalon)
+
+#Porcentaje hombres
+hombresPorcentaje = (hombres*100)/totalSalon
+
+#Porcentaje mujeres
+mujeresPorcentaje = (mujeres*100)/totalSalon
+
+#Imprimir valores
+print ("-----------------------------------------------")
+print ("Total de inscritos:", totalSalon)
+print ("-----------------------------------------------")
+print ("Porcentaje de mujeres:", mujeresPorcentaje,"%")
+print ("Porcentaje de hombres:", hombresPorcentaje,"%")
