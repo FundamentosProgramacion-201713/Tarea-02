424e842d6e5e99fc0a6965c03abc544b442b103d
Carlos Pano <carlospano8@gmail.com>
2017-08-18 19:29:50 -0500

    TODOS los problemas terminados.

--- cuenta.py
+++ cuenta.py
@@ -5,3 +5,25 @@
 
 # A partir de aquí escribe tu programa
 
+#Paso 1: Usuario agrega subototal
+subtotal = input ("Agrega la cantidad a pagar sin IVA y propina:")
+subtotal = int(subtotal)
+
+#Paso 2: Calculo IVA
+iva = (subtotal*116)/100
+
+#Paso 3: Resta del subtotal al IVA
+ivaFinal = (iva - subtotal)
+
+#Paso 4: Calculo propina
+propina = (subtotal*112)/100
+
+#Paso 5: Resta del subtotal a la propina
+propinaFinal = propina - subtotal
+
+#Paso 6: Imprimi suma de subtotal, ivaFinal y propinaFinal
+print ("")
+print ("Propina:", "$", propinaFinal)
+print ("IVA:", "$", ivaFinal)
+print ("----------------------")
+print ("Total a pagar:","$", subtotal + propinaFinal + ivaFinal)
