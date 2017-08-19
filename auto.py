df24b94d9171cd2ccbda0dc6921760d8a2d63587
FERNANDA TORRES <a01746537@itesm.mx>
2017-08-19 15:59:24 -0500

    Buenas tardes, envio la tarea02, excelente fin de semana.

--- auto.py
+++ auto.py
@@ -1,7 +1,22 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: MARIA FERNANDA TORRES VELAZQUEZ, A01746537
+# Descripcion: EL PROGRAMA CALCULA LA DISTANCIA QUE UN AUTO RECORRE EN DETERMINADAS HORAS Y EL TIEMPO QUE TARDA EN RECORRER DETERMINADOS KM.
 
 # A partir de aquí escribe tu programa
+v= float(input ("Ingresa la velocidad (km/h), a la que viaja el auto: "))
+t1=6
+t2= 10
+d= 500
+d1=(t1*v)
+d2= (t2*v)
+t= (d/v)
+
+print ("La distancia que recorre el auto en",t1,"horas es: ",d1, "km")
+print ("La distancia que recorre el auto en",t2,"horas es: ",d2,"km")
+print ("El tiempo que tarda el auto en recorrer", d , " km es: ",t,"horas")
+
+
+
+
 
