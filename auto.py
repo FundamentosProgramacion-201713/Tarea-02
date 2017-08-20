908427284c2e673eaeef5799a4a2e7a5616f275d
miInfo <luisfersurfer14@hotmail.com>
2017-08-19 23:57:11 -0500

    auto completado

--- auto.py
+++ auto.py
@@ -1,7 +1,22 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Luis Fernando Figueroa Rendon, A01746139
+# Descripcion: Pedir la velocidad de un auto para sacar distancia y tiempo.
 
 # A partir de aquí escribe tu programa
 
+velocidad= input("Velocidad del auto en km/h: ")
+
+velocidad= float(velocidad)
+
+d=velocidad*6
+
+d2=velocidad*10
+
+t=500/velocidad
+
+print("Distancia recorrida en 6 horas:", d)
+
+print("Diastancia recorrida en 10 horas:", d2)
+
+print("Tiempo para recorrer 500km:", t)
