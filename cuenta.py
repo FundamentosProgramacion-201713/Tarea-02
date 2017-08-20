10c9df01dface2ba1acdaf55b54ab9a968c34d9b
ACER <neto_1208@hotmail.com>
2017-08-19 22:32:38 -0500

    Trabajo temrinado

--- cuenta.py
+++ cuenta.py
@@ -1,7 +1,15 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Ernesto Ibhar Guevara Gomez, A01746121
+# Descripcion: Imprime el total de la cuenta con Iva y propina.
 
 # A partir de aquí escribe tu programa
+comida=input("Costo de su comida: ")
+comida=int(comida)
 
+propina=comida * 0.12
+iva= comida * 0.16
+TotalAPagar= comida + propina + iva
+print("Propina: ", propina)
+print("IVA", iva)
+print("Total a pagar: ", TotalAPagar)
