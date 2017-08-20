0a8366eafdf9644600cbf934660f5cbed5fb8ca5
miInfo <luisfersurfer14@hotmail.com>
2017-08-20 00:50:47 -0500

    porcentaje completado

--- porcentajes.py
+++ porcentajes.py
@@ -1,7 +1,28 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Luis Fernando Figueroa Rendon, A01746139
+# Descripcion: Sacar el total y porcentaje de hombres y mujeres inscritos.
 
 # A partir de aquí escribe tu programa
 
+hombres= input("Hombres inscritos: ")
+
+mujeres= input("Mujeres inscritas: ")
+
+hombres=float(hombres)
+
+mujeres=float(mujeres)
+
+total= hombres + mujeres
+
+total=float(total)
+
+porcentajeH = (hombres*100) / total
+
+porcentajeM = (mujeres*100) / total
+
+print("Total del inscritos:", total)
+
+print("Porcentaje de hombres:", porcentajeH, "%")
+
+print("Porcentaje de mujeres:", porcentajeM, "%")
