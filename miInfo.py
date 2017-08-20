ede6a5a0bdbc7302d187b53acda2e8357a175342
miInfo <luisfersurfer14@hotmail.com>
2017-08-19 23:15:08 -0500

    miInfo completado

--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,20 @@
 #encoding: UTF-8
 
 # Autor: Luis Fernando Figueroa Rendon
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Descripcion: Dar a conocer algunos datos personales como nombre y gustos.
 
 # A partir de aquí escribe tu programa
 
-print("Nombre: Luis Fernando Figueroa Rendon")   # borrar
+print("Nombre: Luis Fernando Figueroa Rendon")
+
+print("Matricula: A01746139")
+
+print("Carrera: Ingenieria en Sistemas Digitales y Robotica (ISDR)")
+
+print("Escuela de procedencia: Colegio Indoamericano")
+
+print("Descripcion: Me gusta la tecnologia y todo lo relacionado, hago gimnasio y de vez en cuando juego futbol "
+      "y videojuegos con mis amigos.")
+
+
+
