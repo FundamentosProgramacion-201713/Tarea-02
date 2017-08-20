2c881d168e2d93ad9e7a4fc0f54bd8991b15ca4a
miInfo <luisfersurfer14@hotmail.com>
2017-08-20 00:19:30 -0500

    cuenta completado

--- cuenta.py
+++ cuenta.py
@@ -6,7 +6,17 @@
 # A partir de aquí escribe tu programa
 
 costo= input("Costo de la comida: ")
+
 costo= float(costo)
 
-propina= costo*0.13
+propina= costo*0.12
 
+iva= costo*0.16
+
+total= costo + propina + iva
+
+print("Propina: $", propina)
+
+print("IVA: $", iva)
+
+print("Total a pagar: $", total)
