41583a36a244f171992fefd30106d585036b1939
ACER <neto_1208@hotmail.com>
2017-08-19 22:04:48 -0500

    Trabajo temrinado

--- auto.py
+++ auto.py
@@ -1,7 +1,17 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Ernesto Ibhar Guevara Gomez, A01746121
+# Descripcion: Imprime la distancia recorrida en 6 horas y 10 horas ingresando la velocidad. Tambien imprime el tiempo que tarda en recorrer 500 km un auto.
 
 # A partir de aquí escribe tu programa
+ValorDeVelocidad=input("Introduce la velocidad: ")
+velocidad=int(ValorDeVelocidad)
+
+d1= velocidad * 6
+d2= velocidad * 10
+H= 500 / velocidad
+print(d1, "km")
+print(d2, "km")
+print(H, "hrs")
+
 
