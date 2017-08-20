c960d4c6fea8203bcefc998e22af0593825c32c0
Jose Antonio Gomez Mora <ptono98@gmail.com>
2017-08-20 14:48:00 -0500

    Tarea terminada

--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,12 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: José Antonio Gómez Mora, A01374459
+# Descripcion: Imprime mis datos generales. (Nombre, matrícula, carrera, preparatoria y gustos)
 
 # A partir de aquí escribe tu programa
 
-print("Hola")   # borrar
+print("Nombre: José Antonio Gómez Mora")
+print("Matrícula: A01374459")
+print("Carrera: ISDR")
+print("Escuela de procedencia: Prepa Tec, programa Multicultural")
+print("Descripción: Me gusta el deporte, la música, viajar y la tecnología. Practico triatlón y me gusta tocar el piano.")
