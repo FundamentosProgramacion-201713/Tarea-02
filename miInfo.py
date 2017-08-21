432be6c49d03a2ee675127836ede098d24135ff5
Javier Pascal Flores <A01375925@itesm.mx>
2017-08-21 17:48:34 -0500

    Mi info Terminada

--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,18 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Javier Pascal Flores, A01375925
+# Descripcion: Elabora un algoritmo y escribe un programa que muestre en la pantalla la siguiente información:
+# Tu nombre completo.
+# Tu matrícula.
+# Tu carrera.
+# Tu escuela de procedencia (Preparatoria, si es Prepa Tec el programa que cursaste)
+# Dos o tres líneas que te describan de manera general (gustos, habilidades, deportes, libros preferidos, viajes, etc)
 
-# A partir de aquí escribe tu programa
+# A partir de aquí escribe mi programa
 
-print("Hola")   # borrar
+print("Mi información")
+print("Nombre completo: Javier Pascal Flores")
+print("Matricula: A01375925")
+print("Escuela de Procedencia: Prepa Tec CEM Multicultural")
+print("Líneas que me describen: Soy un Cinéfilo apasionado, me gusta la tecnología y amo las ciencias naturales, ecología, salud y vida feliz es lo que busco. Happiness only real when shared")
+print("Gracias")
