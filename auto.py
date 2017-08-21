b6a686505e392e4f14b2f4bcff93be274cae00e2
Javier Pascal Flores <A01375925@itesm.mx>
2017-08-21 18:21:02 -0500

    Auto y Mi info Terminada

--- auto.py
+++ auto.py
@@ -1,7 +1,22 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Javier Pascal Flores, A01375925
+# Descripcion:. La velocidad de un auto puede calcularse con la fórmula v = d/t. (v-velocidad, d-distancia, t-tiempo).
+# Elabora un algoritmo y escribe un programa que pregunte al usuario la velocidad a la que viaja un auto (km/h) y calcule e imprima lo siguiente:
+#La distancia en km. que recorre en 6 hrs.
+#La distancia en km. que recorre en 10 hrs.
+#El tiempo en horas que requiere para recorrer 500 km
 
 # A partir de aquí escribe tu programa
-
+velocidad = input( "¿Cual es la velocidad del auto en kilometros por hora? ")
+type(velocidad)
+velocidad = int(velocidad)
+tiempo=0
+distancia_6=0
+distancia_10=0
+distancia_6=6*velocidad
+distancia_10=10*velocidad
+tiempo=500/velocidad
+print("Distancia en km. que recorre en 6 hrs: ", distancia_6), "km"
+print("Distancia en km. que recorre en 10 hrs: ", distancia_10, "km")
+print("tiempo en horas que requiere para recorrer 500 km: ", tiempo,"hrs")
