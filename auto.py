530bf725edf5a7d8aa2bc31d46f2debc9c73c7f3
Neftalí Rodríguez <nefrodmar@hotmail.com>
2017-08-21 12:48:51 -0500

    Programa de auto completo.

--- auto.py
+++ auto.py
@@ -1,7 +1,22 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Neftalí Rodríguez Martínez, A01375808.
+# Descripción: En ese programa pregunto la velocidad de un auto al usuario y
+# calculo y entrego la distancia recorrida en 6 y 10 horas, así como el tiempo que le llevará recorrer 500 kms.
 
 # A partir de aquí escribe tu programa
 
+print("Este programa pregunta al usuario la velocidad a la que viaja un auto"
+      " e imprime distancia en 6 y 10 horas, así como el tiempo que le llevará "
+      "recorrer 500 kms.")
+
+velocidad = int (input("Escriba la velocidad de su auto: "))
+
+distancia_6hrs = velocidad * 6
+distancia_10hrs = velocidad * 10
+tiempo_500kms = 500 / velocidad
+
+print ("Su velocidad: ", velocidad)
+print ("Distancia recorrida en 6 horas: ", distancia_6hrs)
+print ("Distancia recorrida en 10 horas: ", distancia_10hrs)
+print ("Tiempo que se tarda en recorrer 500 kms: ", tiempo_500kms)
--- miInfo.py
+++ miInfo.py
@@ -1,7 +1,25 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Neftalí Rodríguez Martínez, A01375808.
+# Descripcion: En este programa incluyo información general sobre mí.
 
 # A partir de aquí escribe tu programa
 
+nombrecompleto = "Neftalí Rodríguez Martínez."
+matricula = "A01375808."
+carrera = "Tecnologías de la Información y Electrónica (TIE)."
+escuelaprocedencia = "Prepa Tec, Preparatoria Bicultural."
+descripcion = "Tengo 18 años me gusta el Football americano, " \
+              "me gusta mucho lo relacionado con la tecnología y " \
+              "espero poder aprender en este curso las bases de mi carrera."
+
+print ("Nombre completo:",
+       nombrecompleto)
+print ("Matrícula:",
+       matricula)
+print ("Carrera:",
+       carrera)
+print ("Escuela de Procedencia:",
+       escuelaprocedencia)
+print ("Descripción:",
+       descripcion)
