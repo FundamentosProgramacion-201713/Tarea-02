275dc5a6addd91a3c871eb532e1a73a245e756c5
a01374764 <Edrenash98=>
2017-08-21 20:50:41 +0300

    Problema terminado.

--- coordenadas.py
+++ coordenadas.py
@@ -9,5 +9,6 @@ x = int(input("Indique el valor de x en números enteros: "))
 y = int(input("Indique el valor de y en números enteros: "))
 magnitudr = ((x**2 + y**2)) ** 0.5
 angulo = atan2(y,x)
+anguloGrados = (angulo * 180) / 3.1416
 print ("La magnitud es:", magnitudr)
-print ("El ángulo es:", angulo)
+print ("El ángulo es:", anguloGrados)
