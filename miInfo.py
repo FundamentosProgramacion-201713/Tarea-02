a2789bf0bc66478a9d1538b8e10ce19ed2eb4dac
Juan S. Lozano <A01374452@itesm.mx>
2017-08-21 21:03:50 -0500

    Done

--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,13 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Juan Sebastián Lozano Derbez, A01374452
+# Descripcion: Mostrar mi info.
 
 # A partir de aquí escribe tu programa
+print ("Mi info:")
 
-print ("hola")
+print ("Juan Sebastián Lozano Derbez")
+print ("A01374452")
+print ("ISDR")
+print ("TecMilenio")
+print ("Me gusta la tecnología, la ciencia ficción, leer, las series y películas, la música (en especial la electrónica) y de vez en cuando juego videojuegos.")
