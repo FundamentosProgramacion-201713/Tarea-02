bb7b599dffd031cee58ab66b6376e25d9253e81c
Pablo Garcia <30807776+PabloGarciaM@users.noreply.github.com>
2017-08-21 21:44:29 -0500

    done
    puse el programa

--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,20 @@
-#encoding: UTF-8
+#
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Pablo García Morales, A01745530
+# Descripcion: Queremos un programa donde nos de la informacion mia.
 
-# A partir de aquí escribe tu programa
+Nombre=("Pablo Garcia")
+print(Nombre)
 
-print("Hola")   # borrar
+Matricula=("A01745530")
+print(Matricula)
+
+Carrera=("ISC")
+print(Carrera)
+
+Escuela=("Colegio Moderno Tepeyac")
+print(Escuela)
+
+Descripcion=("Me encanta todo lo que tiene que ver con la tecnología, innovacion tecnologica, los deportes como la natación, basketball")
+print(Descripcion)
+
