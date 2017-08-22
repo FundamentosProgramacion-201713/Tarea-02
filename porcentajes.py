fb6f410fc07adf6e7d74da558ca91b163cb733fe
Ángel Guillermo Ortiz González <a01745998@itesm.mx>
2017-08-21 20:57:04 -0500

    Ejercicio versión pre-final 4.

--- porcentajes.py
+++ porcentajes.py
@@ -1,7 +1,19 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Ángel Guillermo Ortiz González, A01745998
+# Descripcion: Cálculo del porcentaje de hombres y mujeres inscritos en una clase.
 
 # A partir de aquí escribe tu programa
 
+total_mujeres = int(input("Inserte el número de mujeres inscritos: "))
+total_hombres = int(input("Inserte el número de hombres inscritos: "))
+total_inscritos = total_mujeres + total_hombres
+
+porcentaje_mujeres = total_mujeres * 100 / total_inscritos
+porcentaje_hombres = 100 - porcentaje_mujeres
+
+print ("Mujeres inscritas: ", total_mujeres, """
+Hombres inscritos: """, total_hombres, """
+Total de inscritos: """, total_inscritos, """
+Porcentaje de mujeres: """, porcentaje_mujeres,"%", """
+Porcentaje de hombres: """, porcentaje_hombres,"%")
