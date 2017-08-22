a320154c4f306b0ab7e8e9705e2bab5dc7353dbf
Juan S. Lozano <A01374452@itesm.mx>
2017-08-21 21:41:42 -0500

    Done

--- porcentajes.py
+++ porcentajes.py
@@ -1,7 +1,17 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Juan Sebastián Lozano Derbez, A01374452
+# Descripcion: Calcular total de alumnos, numero de hombres y numero de mujeres
 
 # A partir de aquí escribe tu programa
 
+hom = (int(input("Hombres:")))
+muj = (int(input("Mujeres:")))
+
+total = muj + hom
+porhom = hom * 100 / total
+pormuj = 100 - porhom
+
+print("Total:", total)
+print("Porcentaje de hombres:", porhom )
+print("Porcentaje de mujeres:", pormuj)
