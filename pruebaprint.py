a413672adccc12bdcf2071cd5fc1dd6882dbbfa7
Andrea Montero Rivas <30931208+A01374496@users.noreply.github.com>
2017-08-21 19:30:50 -0500

    Update pruebaprint.py

--- pruebaprint.py
+++ pruebaprint.py
@@ -1,1 +1,11 @@
-print ("Cool")
+# Autor: Andrea Montero Rivas
+#Descripcion: Problema 5coordenadas
+
+x=int(input("dame el valor de x",))
+y=int(input("dame el valor de y",))
+
+magnitud=(x**2 +y**2)**0.5
+angulo=math.atan(y/x)
+
+print(magnitud)
+print(angulo)
