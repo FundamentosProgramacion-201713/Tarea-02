3598771b5fc79821f8fcc9c9b50a92eb2dadeef4
Ana María López Soto <30807780+AnaMLopez@users.noreply.github.com>
2017-08-22 15:37:39 -0500

    PROGRAMA 3

--- cuenta.py
+++ cuenta.py
@@ -1,7 +1,17 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Ana María López Soto, A01746134
+# Descripcion: Calcular total a pagar de una comida
 
 # A partir de aquí escribe tu programa
 
+subtotal = int(input("Ingrese el costo de la comida: "))
+
+propina = subtotal * .12
+iva = (subtotal * .16)
+totalPagar = subtotal + propina + iva
+
+print("Costo de su comida: $",subtotal)
+print("Propina: $","%.2f" % propina)
+print("IVA: $","%.2f" %iva)
+print("Total a Pagar: $","%.2f" %totalPagar)
