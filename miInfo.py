6f40041e87ba635e644a0eed525416dd79a1df63
Ángel Guillermo Ortiz González <a01745998@itesm.mx>
2017-08-21 19:29:36 -0500

    Ejercicio versión pre-final.

--- miInfo.py
+++ miInfo.py
@@ -5,4 +5,22 @@
 
 # A partir de aquí escribe tu programa
 
-print("Ángel Guillermo Ortiz González")   # borrar
+nombre_completo = "Angel Guillermo Ortiz Gonzalez"
+matricula = "A01745998"
+carrera = "Ingenieria en Sistemas Digitales y Robotica"
+prepa = "Tecmilenio Campus Cuautitlan"
+gustos = "caminar por las tardes, el olor de los libros nuevos y tocar la guitarra"
+habilidad = "andar en la bicicleta sin manos"
+deporte = "futbol"
+libro_preferido = '"La insoportable levedad del ser" de Milan Kundera'
+viaje = "la playa hace tres veranos"
+
+print ("""Hola. 
+Me llamo:""", nombre_completo, """
+Estudio la carrera de: """, carrera, """
+Estudie en: """, prepa, """
+Me gusta """, gustos, """
+Una de mis habilidades es """, habilidad, """
+El deporte que practico es """, deporte, """
+Uno de mis libros favoritos es """, libro_preferido, """
+Mi último viaje fue a """, viaje)
