f5e13d7245f71bbd65a581722089e52e77fbc067
Anaid Fernanda Labat González <30931249+AnaidL@users.noreply.github.com>
2017-08-21 20:40:03 -0500

    Terminado

--- cuenta.py
+++ cuenta.py
@@ -1,7 +1,17 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+#Autor: Anaid Fernanda Labat González, A01746289
+#Descripción: Encontrar cuanto pago de IVA, cuanto de propina y cuanto pago en total basandonos en el subtotal ingresado
 
-# A partir de aquí escribe tu programa
+#A partir de aqui escribe tu programa
 
+SubTotal=int(input("Costo de su comida"))
+Propina=SubTotal*1.12
+PropinaTotal=Propina-SubTotal
+Iva=SubTotal*1.16
+IvaTotal=Iva-SubTotal
+CostoTotal=SubTotal+PropinaTotal+IvaTotal
+
+print("Propina:","$",PropinaTotal)
+print("IVA:","$",IvaTotal)
+print("Total a pagar", "$", CostoTotal)
