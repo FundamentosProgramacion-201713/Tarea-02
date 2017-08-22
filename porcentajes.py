7b2a3aeb056c808aed994d552b44a9ad2c67f9d6
Anaid Fernanda Labat González <30931249+AnaidL@users.noreply.github.com>
2017-08-21 21:14:44 -0500

    Update porcentajes.py

--- porcentajes.py
+++ porcentajes.py
@@ -6,12 +6,12 @@
 #A partir de aqui escribe tu programa
 
 AlumnosInscritos = int(input("Total de Alumnos Inscritos:"))
-Mujeres=AlumnosInscritos/3
-Hombres=(2*(AlumnosInscritos/3))
+Mujeres=AlumnosInscritos/4
+Hombres=(3*(AlumnosInscritos/4))
 PorcentajeMujeres=((Mujeres*100)/AlumnosInscritos)
 PorcentajeHombres= ((Hombres*100/AlumnosInscritos))
 
-print("Número de Mujeres:", Mujeres)
+print("Número de Mujeres", Mujeres)
 print("Número de Hombres:", Hombres)
 print("Porcentaje de Mujeres: ", PorcentajeMujeres, "%")
 print("Porcentaje de Hombres:", PorcentajeHombres, "%")
