e8577a74262d5dda63c4d2b141491f147c18d315
Ana María López Soto <30807780+AnaMLopez@users.noreply.github.com>
2017-08-22 15:32:47 -0500

    PROGRAMA 2

--- auto.py
+++ auto.py
@@ -1,7 +1,15 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Ana María López Soto, A01746134
+# Descripcion: Cálculos con la fórmula de velocidad
 
 # A partir de aquí escribe tu programa
 
+velocidad = int(input("Tecle la velocidad del automóvil: "))
+distancia6 = 6 * velocidad
+distancia10 = 10 * velocidad
+tiempo500 = 500 / velocidad
+
+print("Distancia recorrida en 6 horas:", distancia6, "kilómetros")
+print("Distancia recorrida en 10 horas:", distancia10, "kilómetros")
+print("Tiempo requerido para recorrer 500 kilómetros:", tiempo500, "horas")
