17080b0337fdf751508131e7b244f4c2f85f0162
Juan S. Lozano <A01374452@itesm.mx>
2017-08-21 21:20:04 -0500

    Done

--- auto.py
+++ auto.py
@@ -1,7 +1,16 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Juan Sebastián Lozano Derbez, A01374452
+# Descripcion: Calcular distancias recorridas y tiempos necesarios para recorrer una distancia usando la velocidad.
 
 # A partir de aquí escribe tu programa
 
+velocidad = int(input("A qué velocidad va el auto?:"))
+
+distancia1 = velocidad*6
+distancia2 = velocidad*10
+tiempo = 500/velocidad
+
+print("Distancia en 6 horas:", distancia1, "km")
+print("Distancia en 10 horas:", distancia2, "km")
+print("Tiempo en el que se recorren 500km:", tiempo, "horas")
