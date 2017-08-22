d3025c7d2ddba3ab17a7eb5123cc0844605cd675
Alejandro Valenzuela Guerrero <30931367+alexA01339478@users.noreply.github.com>
2017-08-22 09:22:40 -0500

    Add files via upload

--- coordenadas.py
+++ coordenadas.py
@@ -5,9 +5,11 @@
 
 # A partir de aquí escribe tu programa
 
+from math import atan2, pi
+
 x = input("Coordenada en x:")
 y = input("Coordenada en y:")
-magnitud = (float (x) **2 + float (y) **2)**(1 / 2)
+magnitud = (float (x) ** 2 + float (y) ** 2)**(0.5)
+angulo = atan2(float(x),float(y))*(180 / pi)
 print("La magnitud es:", magnitud)
-
-
+print("El angulo es:", angulo)
