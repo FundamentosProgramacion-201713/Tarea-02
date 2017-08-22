6d2db2b8e95e2259f7c6d1c61a990d2b5cd8a24b
A01745776 <A01745776@itesm.mx>
2017-08-21 20:47:50 -0500

    Primer ejercicio completo

--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,18 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Eduardo Gallegos, A01745776
+# Descripcion: El algoritmo siguiente proporciona información del autor.
 
 # A partir de aquí escribe tu programa
+nombre = "Eduardo Gallegos Solís"
+carrera = "ISC"
+matricula = "A01745776"
+prepa = "Centro Escolar del Tepeyac"
+descripcion = """Soy un chavo de 18 años que le gusta salir con sus amigos. Me gusta aprender cosas nuevas y ponerme muchos retos.
+entreno taekwondo y me gustan mucho los perros."""
 
-print("Hola chavo")   # borrar
+print("Mi nombre es",nombre)
+print("Mi matrícula es", matricula)
+print("Estudio la carrera",carrera)
+print("Yo vengo de",prepa)
+print(descripcion)
