81544b4a56893585407b8b6d670c1976be6b855a
A01375000@itesm.mx <Charlielugo12>
2017-08-22 15:59:11 -0500

    programa terminado

--- cuenta.py
+++ cuenta.py
@@ -1,7 +1,19 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
+# Autor:Rodrigo Rivera Salinas, A01375000
+# Descripcion: a partir de la cuenta de comida sacar iva, propina y total
 # A partir de aquí escribe tu programa
+
+comida=int(input("dar cuanto costo la comida sin propina"))
+prop=.12
+iva=.16
+propina=comida*prop
+impu=comida*iva
+
+total=comida+propina+impu
+
+print("comida: ",comida)
+print("propina", propina)
+print("iva", impu)
+print("la comida con impuesto y propina es de:", total)
 
