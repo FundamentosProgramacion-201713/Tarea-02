4cbfb9b888962c27ebee5f8149ce7c828ee334fa
Juan S. Lozano <A01374452@itesm.mx>
2017-08-21 21:28:24 -0500

    Done

--- cuenta.py
+++ cuenta.py
@@ -1,7 +1,17 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Juan Sebastián Lozano Derbez, A01374452
+# Descripcion: Calcular el total de la cuenta contando la propina y el IVA.
 
 # A partir de aquí escribe tu programa
 
+nototal = float(input("Costo de la comida: "))
+
+propina = nototal * 12/100
+IVA = nototal * 16/100
+
+total = nototal + propina + IVA
+
+print("Propina", propina)
+print("IVA", IVA)
+print("Total a pagar:", total)
