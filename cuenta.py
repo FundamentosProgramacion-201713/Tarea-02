7c3ad7710c2193687f871b280733f1c5b3c5c0c8
Ángel Guillermo Ortiz González <a01745998@itesm.mx>
2017-08-21 20:28:13 -0500

    Ejercicio versión pre-final 3.

--- cuenta.py
+++ cuenta.py
@@ -1,7 +1,16 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Ángel Guillermo Ortiz González, A01745998
+# Descripcion: Cálculo de porcentajes de propina e impuestos con base en el costo de la comida para obtener el total de la cuenta.
 
 # A partir de aquí escribe tu programa
 
+comida = float(input("Inserte el total de la comida: "))
+propina = comida * 0.12
+iva = comida * 0.16
+total = comida + propina + iva
+
+print ("Costo de su comida: $",comida, """
+Propina: $""",propina, """
+IVA: $""",iva, """
+Total a pagar: $""",total)
