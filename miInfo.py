086212f7db6ec44d3da3e33e4bb27ba8de22e65c
Ana María López Soto <30807780+AnaMLopez@users.noreply.github.com>
2017-08-22 15:37:00 -0500

    PROGRAMA 1

--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,17 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Ana María López Soto, A01746134
+# Descripcion: Datos básicos del alumno
 
 # A partir de aquí escribe tu programa
 
-print("rmr")
+print("Nombre:")
+print("Ana María López Soto")
+print("Matrícula:")
+print("A01746134")
+print("Carrera:")
+print("ISDR")
+print("Escuela de Procedencia:")
+print("TecMilenio")
+print("Descripción:")
+print("Me gusta mucho como ha avanzado la tecnología y siempre quise estudiar algo relacionado con ésta, me gusta mucho leer y correr.")
