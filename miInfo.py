7985865633eadd078c37893ab7e5cbb9aa94ff29
A01375000@itesm.mx <Charlielugo12>
2017-08-22 13:18:28 -0500

    programa terminado

--- miInfo.py
+++ miInfo.py
@@ -1,7 +1,19 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor:Rodrigo Rivera Salinas, A01375000
+# Descripcion:imprimir nombre,matricula,carrera,escuela de procedencia,  gustos,habilidades,etcnombre,matricula,carrera,escuela de procedencia,  gustos,habilidades,etc
 
 # A partir de aquí escribe tu programa
+
+nombre=str("Rodrigo Rivera Salinas")
+matricula=str("A01375000")
+carrera=str("ISDR")
+procedencia=str("Prepa tec")
+gustos=str("jugar soccer,hacer parkour")
+
+print("mi nombre es: ",nombre)
+print("mi matricula es: ",matricula)
+print("mi carrera es: ",carrera)
+print("vengo de: ",procedencia)
+print("mis gustos son: ",gustos)
 
