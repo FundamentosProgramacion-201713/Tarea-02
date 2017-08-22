c9c526e7ff9e66d4051546c6cfded5ee00215e56
Alejandro Valenzuela Guerrero <30931367+alexA01339478@users.noreply.github.com>
2017-08-22 03:19:33 -0500

    Add files via upload

---
+++ coordenadas.py
@@ -0,0 +1,13 @@
+#encoding: UTF-8
+
+# Autor: Alejandro Valenzuela Guerrero, A01339478
+# Descripcion: Dando x y y convertirlas a coordenadas polares
+
+# A partir de aquí escribe tu programa
+
+x = input("Coordenada en x:")
+y = input("Coordenada en y:")
+magnitud = (float (x) **2 + float (y) **2)**(1 / 2)
+print("La magnitud es:", magnitud)
+
+
--- porcentajes.py
+++ porcentajes.py
@@ -1,7 +1,16 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Alejandro Valenzuela Guerrero, A01339478
+# Descripcion: Total de hombres y mujeres inscritos y porcentaje de hombres y mujeres
 
 # A partir de aquí escribe tu programa
+
+nhombres = input("Numero de hombres inscritos:")
+nmujeres = input("Numero de mujeres inscritas:")
+total = int (nhombres) + int (nmujeres)
+porcentajeh = (100 / total) * int (nhombres)
+porcentajem = (100 / total) * int (nmujeres)
+print("Total de inscritos:", total, "personas")
+print("Porcentaje de hombres:", porcentajeh, "%")
+print("Porcentaje de mujeres:", porcentajem, "%")
 
