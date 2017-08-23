3a427e1c90394bb7d9a3428bb115eeec8fe0a616
Alberto López <elOXXO@users.noreply.github.com>
2017-08-23 00:18:23 -0500

    ¡Ya acabé!

--- auto.py
+++ auto.py
@@ -1,7 +1,20 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Alberto López Reyes, A01745811
+# Descripcion: Este programa calcula, basándose en la velocidad otorgada,
+# la distancia del auto en 6 horas y 10 horas, y el tiempo en 500 km.
 
 # A partir de aquí escribe tu programa
 
+intVelocidad = int(input("Velocidad del auto en kh/h: "))
+
+intTiempo_500km = 0
+fltTiempo_500km = float(intTiempo_500km)
+
+intDistancia_6hrs = intVelocidad * 6
+intDistancia_10hrs = intVelocidad * 10
+fltTiempo_500km = 500 / float(intVelocidad)
+
+print(str("Distancia recorrida en 6hrs: " + str(intDistancia_6hrs) + " km"))
+print(str("Distancia recorrida en 10hrs: " + str(intDistancia_10hrs) + " km"))
+print(str("Tiempo para recorrer 500 km: " + str(fltTiempo_500km) + " hrs."))
