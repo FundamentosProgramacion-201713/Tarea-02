244bf50bb3fcd6a416419fcc5e37e02833d29c4b
Dora Lizárraga <30931228+a01229599@users.noreply.github.com>
2017-08-25 11:17:16 -0500

    Add files via upload

--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,19 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Dora Gabriela Lizarraga Gonzalez, A01229599
+# Descripcion: En este progarma escribire mis datos.
 
 # A partir de aquí escribe tu programa
 
-print("Hola")   # borrar
+print('Nombre:')
+print('Dora Gabriela Lizarraga Gonzalez')
+print('Matricula:')
+print('A01229599')
+print('Carrera:')
+print('ISC')
+print('Escuela de procedencia:')
+print('Prepa Tec, programa Multicultural')
+print('Descripcion:')
+print('Me gusta aprender idiomas y de otras culturas, sobretodo viajar a donde sucedio la historia.')
+print('Tambien disfruto de jugar videojuegos en mis ratos libres y escuchar musica.')
+
