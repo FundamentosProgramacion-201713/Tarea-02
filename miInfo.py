a167b5f02b46f96f744a35eb5f339009e8fd3a96
Javier León <A01745532@itesm.mx>
2017-08-29 16:45:23 -0500

    Tarea 2 Completa

--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,18 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Javier León Alcántara , A01745532
+# Descripcion: Imprime información
 
-# A partir de aquí escribe tu programa
 
-print("Javier")
+print("NOMBRE:"
+      "Javier León Alcántara")
+print("MATRICULA:"
+      "A01745532")
+print("CARRERA:"
+      "ITI")
+print("ESCUELA DE PROCEDENCIA:"
+      "International College Sor Juana Inés de la Cruz")
+print("DESCRIPCIÓN:"
+      "Soy un fanático de la tecnologia, los gadgets y la innovacion,",
+      "mis pasatiempos favoritos es tocar instrumentos musicales y salir",
+      "con mi familia")
