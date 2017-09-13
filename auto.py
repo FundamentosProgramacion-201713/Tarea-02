45a02f371ddef7966b55b460d63f13f611f8be5e
Viviana Osorio Nieto <54321vivi@gmail.com>
2017-09-13 14:10:51 -0500

    tarea auto.py terminada.

--- auto.py
+++ auto.py
@@ -1,7 +1,17 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: VivianaOsorioNieto, A01374461
+# Descripcion: CalcularLa la distancia y tiempo de un auto en relacion con su velocidad.
 
-# A partir de aquí escribe tu programa
+v = int(input("cual es la velocidad del auto en kh/h?"))
+        
+d1 = 6 * v
+print (" la distancia recorrida en 6 horas será: ", d1)
 
+d2 = 10 * v
+print ("la distancia recorrida en 10 horas será: " d2)
+        
+t = 500/ v
+print ("el tiempo que se tarda en recorrer 500km es: ", t)
+        
+        
