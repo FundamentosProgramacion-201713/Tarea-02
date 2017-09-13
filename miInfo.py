47ac12e30b170a85dd5c129515ff3bd635eaf2c1
Viviana Osorio Nieto <54321vivi@gmail.com>
2017-09-13 13:58:07 -0500

    miInfo terminada

--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,12 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
-# A partir de aquí escribe tu programa
+# Autor: VivianaOsorioNieto, A01374461
+# Descripcion: MostrarInformacionSobreMi.
+ 
+print("Viviana osorio Nieto")
+print("A01374461")
+print("ISDR")
+print ("PrepaTEC esmeralda")
+print (" Soy una persona creativa y me gusta mucho la musica, tengo facilidad con instrumentos musicales ya que toco varios. me gusta spiderman, los gatos y el chocolate")
 
 print("rmr")
